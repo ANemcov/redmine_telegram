9dc35382d0d125833efae438938db53a8341d420
Alexandr Nemcov <alexnemcov@yandex.ru>
2016-01-24 18:25:47 +0300

    Version up. lisner deleted

--- init.rb
+++ init.rb
@@ -2,15 +2,13 @@ require 'redmine'
 
 require 'telegram_mailer_patch'
 
-require_dependency 'redmine_telegram/listener'
-
 Redmine::Plugin.register :redmine_telegram do
 	name 'Redmine Telegram'
 	author 'Alex Nemtsov aka pythonchik'
 	url 'https://github.com/ANemcov/redmine_telegram'
 	author_url 'http://cmd-q.ru'
 	description 'Telegram chat integration'
-	version '0.2'
+	version '0.3'
 
 	requires_redmine :version_or_higher => '0.8.0'
 
